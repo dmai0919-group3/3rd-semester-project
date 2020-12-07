53c24f7e5d19cb6d9e1c6aecffcb8080e8fa6aa2
fitch-io <42519577+fitch-io@users.noreply.github.com>
2020-12-07 19:04:26 +0100

    UI Improvements and Folder mechanics

--- Group3.Semester3.DesktopClient/Model/MainWindowModel.cs
+++ Group3.Semester3.DesktopClient/Model/MainWindowModel.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Group3.Semester3.DesktopClient.Model
 {
@@ -10,7 +12,7 @@ namespace Group3.Semester3.DesktopClient.Model
         public class FileEntityWrapper
         {
             public FileEntity FileEntity { get; set; }
-            public string Icon { get; set; }
+            public BitmapSource Icon { get; set; }
             public bool Selected { get; set; }
         }
         public class DownloadTask
--- Group3.Semester3.DesktopClient/Views/MainWindow.xaml.cs
+++ Group3.Semester3.DesktopClient/Views/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Group3.Semester3.DesktopClient.Model;
 using static Group3.Semester3.DesktopClient.Model.MainWindowModel;
+using MaterialDesignThemes.Wpf;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Group3.Semester3.DesktopClient.Views
 {
@@ -29,6 +32,16 @@ namespace Group3.Semester3.DesktopClient.Views
 
         private MainWindowModel Model = new Model.MainWindowModel();
 
+        private BitmapImage IconFolder;
+        private BitmapImage IconFolderOpen;
+        private BitmapImage IconFile;
+        private BitmapImage IconText;
+        private BitmapImage IconMusic;
+        private BitmapImage IconImage;
+        private BitmapImage IconExecutable;
+
+        Stack<FileEntity> folderStack = new Stack<FileEntity>();
+
         public FileEntityWrapper SelectedFile { get; set; }
 
         public MainWindow(ApiService apiService, Switcher switcher)
@@ -40,19 +53,104 @@ namespace Group3.Semester3.DesktopClient.Views
 
             InitializeComponent();
 
-            labelUserName.Content = $"{apiService.User.Name} ({apiService.User.Email})".ToUpper();
+            // ono
+            IconFolder = Resources["IconFolder"] as BitmapImage;
+            IconExecutable = Resources["IconExecutable"] as BitmapImage;
+            IconText = Resources["IconText"] as BitmapImage;
+            IconMusic = Resources["IconMusic"] as BitmapImage;
+            IconFile = Resources["IconFile"] as BitmapImage;
+            IconImage = Resources["IconImage"] as BitmapImage;
+            IconFolderOpen = Resources["IconFolderOpen"] as BitmapImage;
 
-            foreach (FileEntity f in apiService.FileList())
-            {
-                Model.FileViewList.Add(new FileEntityWrapper { FileEntity = f });
-            }
+            labelUserName.Content = $"{apiService.User.Name} ({apiService.User.Email})".ToUpper();
 
             UpdateFilePanel(new FileEntity
             {
                 Name = "Test Set.xml"
             });
 
+            UpdateFileList();
             UpdateProgress();
+        }
+
+        private void UpdateFileList(FileEntity rootFolder = null)
+        {
+            Model.FileViewList.Clear();
+
+            var l = (rootFolder == null) ? apiService.FileList() : apiService.FileList(rootFolder.Id);
+
+            if (rootFolder != null)
+            {
+
+
+                var e = new FileEntityWrapper
+                {
+                    Icon = IconFolderOpen,
+                    FileEntity = rootFolder
+                };
+                Model.FileViewList.Add(e);
+            }
+
+            // TODO the stack is only to keep track of the folder path. def fix this, it won't work when opening random folders
+
+            if (folderStack.Count > 0 && folderStack.Peek().Id == rootFolder?.Id)
+            {
+                folderStack.Pop();
+            }
+            else if (rootFolder != null) folderStack.Push(rootFolder);
+
+            {
+                string name = "Home";
+                foreach (var x in folderStack)
+                {
+                    name += $" / {x.Name}";
+                }
+                labelPath.Content = name;
+            }
+
+            foreach (FileEntity f in l.Where(x => x.IsFolder).OrderBy(x => x.Name))
+            {
+                Model.FileViewList.Add(new FileEntityWrapper { 
+                    FileEntity = f,
+                    Icon = IconFolder
+                });
+            }
+
+            foreach (FileEntity f in l.Where(x => !x.IsFolder).OrderBy(x => x.Name))
+            {
+                var e = new FileEntityWrapper { FileEntity = f };
+
+                if (f.Name.EndsWith(".mp3") ||
+                    f.Name.EndsWith(".wav") ||
+                    f.Name.EndsWith(".ogg") ||
+                    f.Name.EndsWith(".flac")) e.Icon = IconMusic;
+                else if (
+                    f.Name.EndsWith(".txt") ||
+                    f.Name.EndsWith(".doc") ||
+                    f.Name.EndsWith(".docx") ||
+                    f.Name.EndsWith(".odt")) e.Icon = IconText;
+                else if (
+                    f.Name.EndsWith(".exe") ||
+                    f.Name.EndsWith(".cmd") ||
+                    f.Name.EndsWith(".bat") ||
+                    f.Name.EndsWith(".com") ||
+                    f.Name.EndsWith(".vbs")) e.Icon = IconExecutable;
+                else if (
+                    f.Name.EndsWith(".bmp") ||
+                    f.Name.EndsWith(".jpg") ||
+                    f.Name.EndsWith(".jpeg") ||
+                    f.Name.EndsWith(".psd") ||
+                    f.Name.EndsWith(".tiff") ||
+                    f.Name.EndsWith(".gif") ||
+                    f.Name.EndsWith(".gif") ||
+                    f.Name.EndsWith(".png")) e.Icon = IconImage;
+                else
+                    e.Icon = IconFile;
+
+                Model.FileViewList.Add(e);
+            }
+
+            if(rootFolder != null) rootFolder.Name = "[UP]";
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -72,17 +170,35 @@ namespace Group3.Semester3.DesktopClient.Views
         private void HandleDoubleClick(object sender, MouseButtonEventArgs e)
         {
             FileEntityWrapper selected = ((ListViewItem)sender).Content as FileEntityWrapper;
-            if (!selected.FileEntity.IsFolder) FileDownloadAndExecAsync(selected.FileEntity);
+            if (!selected.FileEntity.IsFolder)
+            {
+                _ = FileDownloadAndExecAsync(selected.FileEntity);
+            }
+            else
+            {
+                UpdateFilePanel(selected.FileEntity);
+                UpdateFileList(selected.FileEntity);
+            }
         }
 
         private void MenuItemSave_Click(object sender, RoutedEventArgs e)
         {
-            if (SelectedFile != null && !SelectedFile.FileEntity.IsFolder) FileSaveAsAsync(SelectedFile.FileEntity);
+            if (SelectedFile != null && !SelectedFile.FileEntity.IsFolder) _ = FileSaveAsAsync(SelectedFile.FileEntity);
         }
 
         private void MenuItemRun_Click(object sender, RoutedEventArgs e)
         {
-            if (SelectedFile != null && !SelectedFile.FileEntity.IsFolder) FileDownloadAndExecAsync(SelectedFile.FileEntity);
+            if (SelectedFile != null && !SelectedFile.FileEntity.IsFolder) _ = FileDownloadAndExecAsync(SelectedFile.FileEntity);
+        }
+
+        private void MenuItemRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedFile != null && !SelectedFile.FileEntity.IsFolder) apiService.DeleteFile(SelectedFile.FileEntity);
+        }
+
+        private void MenuItemNewFolder_Click(object sender, RoutedEventArgs e)
+        {
+            //var result = DialogHost.Show(new String("asdf"));
         }
     }
 }
